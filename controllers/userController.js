3866ba1be63612f62c74f50f2f617ce1d0f47ff4
Andrew Yoo <andrewbyoo@gmail.com>
2022-02-07 17:47:57 -0500

    Added find all users

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,7 +1,11 @@
 const { User } = require('../models');
 
 module.exports = {
-  getUsers(req, res) {},
+  getUsers(req, res) {
+    User.find()
+      .then((users) => res.status(200).json(users))
+      .catch((err) => res.status(500).json(err));
+  },
   createUser(req, res) {},
   getUserById(req, res) {},
   updateUser(req, res) {},
