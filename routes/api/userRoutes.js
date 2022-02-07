e8e625a40eac3983de4e92f8e73c9dfcc33b66be
Andrew Yoo <andrewbyoo@gmail.com>
2022-02-07 17:21:07 -0500

    Added template routes

--- routes/api/userRoutes.js
+++ routes/api/userRoutes.js
@@ -1,6 +1,18 @@
 const router = require('express').Router();
 const {
-  // Destructured components in controller
+  // TODO: Add destructured components in controller
 } = require('../../controllers/userController');
+
+// TODO: Add controller component into get and post
+router.route('/').get().post();
+
+// TODO: Add controller component into get, put, and delete
+router.route('/:userId').get().put().delete();
+
+// TODO: Add controller component into post
+router.route('/:userId/friends').post();
+
+// TODO: Add controller component into delete
+router.route('/:userId/friends/:friendId').delete();
 
 module.exports = router;
