c2e85fa7672eb6f9f12643ed227e44ef1a370bdf
Andrew Yoo <andrewbyoo@gmail.com>
2022-02-07 17:38:19 -0500

    Added template controller functions

--- controllers/thoughtController.js
+++ controllers/thoughtController.js
@@ -1,5 +1,11 @@
 const { User, Thought } = require('../models');
 
 module.exports = {
-
-}
+  getThoughts(req, res) {},
+  createThought(req, res) {},
+  getThoughtById(req, res) {},
+  updateThought(req, res) {},
+  deleteThought(req, res) {},
+  addReaction(req, res) {},
+  deleteReaction(req, res) {},
+};
