a66596d72ce7e417464b8e9b93c0672277727537
Andrew Yoo <andrewbyoo@gmail.com>
2022-02-07 17:26:57 -0500

    Added template for thought api routes

--- routes/api/thoughtRoutes.js
+++ routes/api/thoughtRoutes.js
@@ -3,4 +3,16 @@ const {
   // TODO: Add destructured components in controller
 } = require('../../controllers/thoughtController');
 
+// TODO: Add controller component into get and post
+router.route('/').get().post();
+
+// TODO: Add controller component into get, put, and delete
+router.route('/:thoughtId').get().put().delete();
+
+// TODO: Add controller component into post
+router.route('/:thoughtId/reactions').post();
+
+// TODO: Add controller component into delete
+router.route('/:thoughtId/reactions/:reactionId');
+
 module.exports = router;
