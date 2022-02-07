28eb1b401f13b4f6692ecf2120736a174430407d
Andrew Yoo <andrewbyoo@gmail.com>
2022-02-07 17:38:54 -0500

    Removed todo comments

--- routes/api/userRoutes.js
+++ routes/api/userRoutes.js
@@ -1,6 +1,5 @@
 const router = require('express').Router();
 const {
-  // TODO: Add destructured components in controller
   getUsers,
   createUser,
   getUserById,
@@ -10,16 +9,9 @@ const {
   deleteFriend
 } = require('../../controllers/userController');
 
-// TODO: Add controller component into get and post
 router.route('/').get(getUsers).post(createUser);
-
-// TODO: Add controller component into get, put, and delete
 router.route('/:userId').get(getUserById).put(updateUser).delete(deleteUser);
-
-// TODO: Add controller component into post
 router.route('/:userId/friends').post(addFriend);
-
-// TODO: Add controller component into delete
 router.route('/:userId/friends/:friendId').delete(deleteFriend);
 
 module.exports = router;
