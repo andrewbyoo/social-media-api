6fe0d0f1a0bf016f5c582e8f6091364af18711a9
Andrew Yoo <andrewbyoo@gmail.com>
2022-02-07 17:33:42 -0500

    Added template controller functions

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,5 +1,11 @@
 const { User } = require('../models');
 
 module.exports = {
-
+  getUsers(req, res) {},
+  createUser(req, res) {},
+  getUserById(req, res) {},
+  updateUser(req, res) {},
+  deleteUser(req, res) {},
+  addFriend(req, res) {},
+  deleteFriend(req, res) {},
 };
