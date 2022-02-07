5d42676f308f3b81ae38df3030444ace98b83b82
Andrew Yoo <andrewbyoo@gmail.com>
2022-02-07 15:20:31 -0500

    Added user schema

---
+++ models/User.js
@@ -0,0 +1,19 @@
+const { Schema, model } = require('mongoose');
+const reactionSchema = require('./Reaction');
+
+const userSchema = new Schema(
+  {
+    username: {
+      type: String,
+      unique: true,
+      required: true,
+      trim: true,
+    },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+      match: /^([a-z0-9_\.-]+)@([\da-z\.-]+)\.([a-z\.]{2,6})$/,
+    },
+  }
+)
