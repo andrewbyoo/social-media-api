49a0b3b07b79357c558f73e740dad9bf73e481f1
Andrew Yoo <andrewbyoo@gmail.com>
2022-02-08 18:26:13 -0500

    Added connection function that drops the collections if they exist

--- seeds/seed.js
+++ seeds/seed.js
@@ -1,2 +1,15 @@
 const connection = require('../config/connection');
-const { User, Thought, Reaction } = require('../models');
+const { User, Thought } = require('../models');
+
+const userData = require('./userData.json');
+const thoughtData = require('./thoughtData.json');
+const reactionData = require('./thoughtData.json');
+
+connection.on('error', (err) => err);
+
+connection.once('open', async () => {
+  console.log('connected');
+
+  await Thought.deleteMany({});
+  await User.deleteMany({});
+});
