9d4415c0318496867e656626d8b9a11cd56aa6d1
Andrew Yoo <andrewbyoo@gmail.com>
2022-02-08 19:45:11 -0500

    Moved json data to variables

---
+++ seeds/data.js
@@ -0,0 +1,45 @@
+const userData = [
+  {
+    "username": "Johnny",
+    "email": "JSilverhand@cyberpunk.com"
+  },
+  {
+    "username": "Thomas",
+    "email": "neo@thematrix.com"
+  },
+  {
+    "username": "John",
+    "email": "johnwick@example.com"
+  },
+  {
+    "username": "Andrew",
+    "email": "andrewbyoo@gmail.com"
+  },
+  {
+    "username": "Keanu",
+    "email": "keanureeves@example.com"
+  }
+]
+
+const thoughtData = [
+  {
+    "thoughtText": "Why am I inside this person's head?",
+    "username": "Johnny"
+  },
+  {
+    "thoughtText": "Where is my guitar?",
+    "username": "Johnny"
+  },
+  {
+    "thoughtText": "Guns. LOTS of guns.",
+    "username": "Thomas"
+  },
+  {
+    "thoughtText": "Everything's got a price.",
+    "username": "John"
+  },
+  {
+    "thoughtText": "The simple act of paying attention can take you a long way.",
+    "username": "Keanu"
+  }
+]
