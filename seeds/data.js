2f05eef8b2b2f2de71de797bab19be2d8a2e1e8d
Andrew Yoo <andrewbyoo@gmail.com>
2022-02-08 19:53:28 -0500

    Imported ObjectId method from mongodb npm packag, added hard coded object ids, and exported both data variables

--- seeds/data.js
+++ seeds/data.js
@@ -1,21 +1,28 @@
+const ObjectId = require('mongodb').ObjectId;
+
 const userData = [
   {
+    "_id": ObjectId("62030f6c64fba167d8adc058"),
     "username": "Johnny",
     "email": "JSilverhand@cyberpunk.com"
   },
   {
+    "_id": ObjectId("62030f6c64fba167d8adc059"),
     "username": "Thomas",
     "email": "neo@thematrix.com"
   },
   {
+    "_id": ObjectId("62030f6c64fba167d8adc05a"),
     "username": "John",
     "email": "johnwick@example.com"
   },
   {
+    "_id": ObjectId("62030f6c64fba167d8adc05b"),
     "username": "Andrew",
     "email": "andrewbyoo@gmail.com"
   },
   {
+    "_id": ObjectId("62030f6c64fba167d8adc05c"),
     "username": "Keanu",
     "email": "keanureeves@example.com"
   }
@@ -23,23 +30,30 @@ const userData = [
 
 const thoughtData = [
   {
+    "_id": ObjectId("62030f6c64fba167d8adc052"),
     "thoughtText": "Why am I inside this person's head?",
     "username": "Johnny"
   },
   {
+    "_id": ObjectId("62030f6c64fba167d8adc053"),
     "thoughtText": "Where is my guitar?",
     "username": "Johnny"
   },
   {
+    "_id": ObjectId("62030f6c64fba167d8adc054"),
     "thoughtText": "Guns. LOTS of guns.",
     "username": "Thomas"
   },
   {
+    "_id": ObjectId("62030f6c64fba167d8adc055"),
     "thoughtText": "Everything's got a price.",
     "username": "John"
   },
   {
+    "_id": ObjectId("62030f6c64fba167d8adc056"),
     "thoughtText": "The simple act of paying attention can take you a long way.",
     "username": "Keanu"
   }
 ]
+
+module.exports = { userData, thoughtData }
