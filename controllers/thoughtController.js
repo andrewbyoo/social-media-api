9a47cd0e5fe08717e7c6819012aef2dd0d5ddbb9
Andrew Yoo <andrewbyoo@gmail.com>
2022-02-08 23:44:29 -0500

    Added select to omit __v and added update to add the thought to the user

--- controllers/thoughtController.js
+++ controllers/thoughtController.js
@@ -3,16 +3,26 @@ const { User, Thought } = require('../models');
 module.exports = {
   getThoughts(req, res) {
     Thought.find()
+      .select('-__v')
       .then((thoughts) => res.status(200).json(thoughts))
       .catch((err) => res.status(500).json(err));
   },
   createThought(req, res) {
     Thought.create(req.body)
-      .then((newThought) => res.status(200).json(newThought))
+      .then((newThought) => {
+        User.findOneAndUpdate(
+          { username: req.body.username },
+          { $addToSet: { thoughts: newThought._id } },
+          { new: true }
+        )
+          .then((newThought) => res.status(200).json(newThought))
+          .catch((err) => res.status(500).json(err));
+      })
       .catch((err) => res.status(500).json(err));
   },
   getThoughtById(req, res) {
     Thought.findOne({ _id: req.params.thoughtId })
+      .select('-__v')
       .then((thought) =>
         !thought
           ? res.status(400).json({ message: 'No thought with that ID' })
