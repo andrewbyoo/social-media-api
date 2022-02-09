aa759ebf74fc91dbc78f46d7569a8e98a7ee9a85
Andrew Yoo <andrewbyoo@gmail.com>
2022-02-08 21:10:29 -0500

    Fixed typo

--- controllers/userController.js
+++ controllers/userController.js
@@ -29,7 +29,7 @@ module.exports = {
   updateUser(req, res) {
     User.findOneAndUpdate(
       { _id: req.params.userId },
-      { $set: reg.body },
+      { $set: req.body },
       { runValidators: true, new: true }
     )
       .then((updatedUser) =>
